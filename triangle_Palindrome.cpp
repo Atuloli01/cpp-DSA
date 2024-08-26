5748f6ff66ad864cb71a8be869a735804461ca3c
🧭Carpediam ¡! <125664476+Atuloli01@users.noreply.github.com>
2024-08-26 14:51:32 +0530

    Update triangle_Palindrome.cpp

--- triangle_Palindrome.cpp
+++ triangle_Palindrome.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+// only steps to do that question
+/* #include <iostream>
 using namespace std;
 int main() {
   int number;
@@ -7,6 +8,7 @@ int main() {
   for (int row = 0; row < number; row++) {
     int startNum = 1;
     for (int column = 0; column < start; column++) {
+      // this will print left side in the pyramid
       if (column < number - row - 1) {
         cout << " ";
       } else {
@@ -15,12 +17,13 @@ int main() {
         startNum++;
       }
     }
+    start++; // 2nd step to print another side of palindrome pyramid...
     cout << endl;
   }
 }
+*/
 
-
-// CASE II
+// III step main code of this question
 #include <iostream>
 using namespace std;
 int main() {
@@ -28,16 +31,24 @@ int main() {
   cin >> number;
   int start = number;
   for (int row = 0; row < number; row++) {
-    int startNum = 1;
+    int startNum = 1; // 2nd step part...
     for (int column = 0; column < start; column++) {
+      // this will print left side in the pyramid
       if (column < number - row - 1) {
         cout << " ";
+      } else if (column <= number - 1) {
+        cout << startNum;
+        startNum++;
+      } else if (column == number) {
+        startNum = startNum - 2;
+        cout << startNum;
+        startNum--;
       } else {
-        // cout << "*";
-        cout << startNum << " ";
+        cout << startNum;
         startNum++;
       }
     }
+    start++; // 2nd step to print another side of palindrome pyramid...
     cout << endl;
   }
 }
